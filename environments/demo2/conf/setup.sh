1c430556be9b6108b6197c4adf81d2324032e41b
Nick Larson <nicklas.larson@gmail.com>
2018-09-09 14:59:35 -0500

    cleanup demo1

--- environments/demo2/conf/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo yum update -y
-
-sudo yum install nginx -y
-sudo chkconfig nginx on
-sudo service nginx start
--- modules/loadbalancer/output.tf
+++
@@ -1,7 +0,0 @@
-output "target_group_arn" {
-  value = "${aws_alb_target_group.lb_tg.arn}"
-}
-
-output "security_group_id" {
-  value = "${aws_security_group.lb_sg.id}"
-}
